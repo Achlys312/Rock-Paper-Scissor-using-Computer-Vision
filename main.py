5808dc383ca90043b75a18ab94e9e66f2b6794b2
Achlys312 <yadavgarv312@gmail.com>
2023-06-21 15:54:56 +0530

    commit1

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import cvzone
 
 # Code for opening the webcam
 cap = cv2.VideoCapture(0)
+cap.set(3,640)
+cap.set(4,480)
 
 while True:
+    imgBG = cv2.imread("Resources/bg.jpg")
+
+
     success, img = cap.read()
     cv2.imshow("Image", img)
+    cv2.imshow("bg", imgBG)
     cv2.waitKey(1)
